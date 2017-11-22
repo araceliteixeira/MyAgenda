adcd5630669d9c7b3ae9ee5d4f4fab0a2d5de180
MacStudent <macstudent@20X-iMacXX-M-6.local>
2017-11-22 15:29:59 -0500

    End of class 3

--- app/src/main/java/com/ant/myagenda/FormActivity.java
+++ app/src/main/java/com/ant/myagenda/FormActivity.java
@@ -2,6 +2,9 @@ package com.ant.myagenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,12 +16,31 @@ public class FormActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
 
-        Button saveButton = (Button) findViewById(R.id.form_save);
+        /*Button saveButton = (Button) findViewById(R.id.form_save);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(FormActivity.this, "Button was clicked", Toast.LENGTH_SHORT).show();
+
             }
-        });
+        });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_form, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_form_ok:
+                Toast.makeText(FormActivity.this, "Student Saved", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/ant/myagenda/StudentListActivity.java
+++ app/src/main/java/com/ant/myagenda/StudentListActivity.java
@@ -1,8 +1,11 @@
 package com.ant.myagenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class StudentListActivity extends AppCompatActivity {
@@ -12,9 +15,18 @@ public class StudentListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_list);
 
-        String[] students = {"Marcos", "John", "Peter", "Alfred", "Melinda"};
+        String[] students = {"Marcos", "John", "Peter", "Alfred", "Melinda", "Sonia", "Marc", "Paul", "Lana", "Caleb", "Diana"};
         ListView studentsList = (ListView) findViewById(R.id.students_list);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, students);
         studentsList.setAdapter(adapter);
+
+        Button newStudent = (Button) findViewById(R.id.students_add);
+        newStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentGotoForm = new Intent(StudentListActivity.this, FormActivity.class);
+                startActivity(intentGotoForm);
+            }
+        });
     }
 }
